33760fff759840fffa6781118eb6e30507f1c33e
James Stuart Milne <jstuartmilne@gmail.com>
2023-08-04 15:26:10 -0300

    chore: docs update

--- src/banking/workers/workers.ts
+++ src/banking/workers/workers.ts
@@ -1,7 +1,7 @@
 import { ConductorWorker } from "@io-orkes/conductor-javascript";
 import * as fraudService from "../services/fraud-service";
 
- // docs-marker-start-1
+// docs-marker-start-1
 export const fraudCheckWorker: ConductorWorker = {
   taskDefName: "fraud-check",
   execute: async ({ inputData }) => {
@@ -13,5 +13,8 @@ export const fraudCheckWorker: ConductorWorker = {
       status: "COMPLETED",
     };
   },
+  domain: "fraud", // Optional
+  pollInterval: 100, // Optional can be specified in the TaskManager
+  concurrency: 2, // Optional can be specified in the TaskManager
 };
 // docs-marker-end-1
