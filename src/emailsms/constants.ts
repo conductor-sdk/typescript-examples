3f96295667a53532ceb9f84425071ff9fff3c582
James Stuart Milne <jstuartmilne@gmail.com>
2024-06-26 12:51:16 -0300

    feat: merge

--- src/emailsms/constants.ts
+++ src/emailsms/constants.ts
@@ -1,4 +1,4 @@
 export const GET_USER_INFO = "get_user_info";
 export const SEND_EMAIL = "send_email";
 export const SEND_SMS = "send_sms";
-export const COMPLEX_WORKFLOW_NAME = "user_notification";
+export const COMPLEX_WORKFLOW_NAME = "user_notification_2";
